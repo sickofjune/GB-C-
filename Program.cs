2f92e02a12fd47ebe04f4f6a32a5546938fd5419
Egor <egorkozlov1810@gmail.com>
2023-07-16 02:13:28 +0400

    firstTry

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Куб числа
+// int func (int arg){
+//     int number = 1;
+//     for (int i = 1; i <= arg; i++)
+//     {
+//         number = i*i*i;
+//         Console.WriteLine(number);
+//     }
+//     return number;
+// }
+// Console.Write("Число ");
+// int number = func(int.Parse(Console.ReadLine()));
+
+
+// Сколько цифр в числе
+// int num = int.Parse(Console.ReadLine());
+// int count = 0;
+// while(num > 0){
+//     count++;
+//     num /= 10;
+// }
+// Console.WriteLine("Цифры: " + count);
+
+// Факториал с суммой ЛОЛ
+// int num = int.Parse(Console.ReadLine());
+// int result = 0;
+// for (int i = 0; i <= num; i++)
+// {
+//     result = result + i;
+// }
+// Console.WriteLine(result);
+
+// Факториал
+// int num = int.Parse(Console.ReadLine());
+// int result = 1;
+// for (int i = 1; i <= num; i++)
+// {
+//     result = result * i;
+// }
+// Console.WriteLine(result);
+
+// Рандомный массив 
+// int[] massa = new int[8];
+// for (int i = 0; i < massa.Length; i++)
+// {
+//     massa[i] = new Random().Next(0,2);
+// }
+// Console.WriteLine($"[{string.Join(", ", massa)}]");
+
