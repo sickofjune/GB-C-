995f56e0159455eff5b0caa5b9a9d134c66fd019
Egor <egorkozlov1810@gmail.com>
2023-07-18 23:25:55 +0400

    Семинар 5

--- Program.cs
+++ Program.cs
@@ -1,43 +1,50 @@
-﻿// ЗАДАНИЯ ТРЕТЬЕГО СЕМИНАРА ↓↓↓↓↓
-
-//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
-
-// int num = int.Parse(Console.ReadLine());
-// if (num % 10 == num / 10000 && (num % 100)/10 == (num / 1000)%10){
-//     Console.WriteLine("Да");
+﻿// int[] generateArray(int lenght, int minValue, int maxValue)  // Генерация массива
+// {
+//     int [] arr = new int[lenght];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(minValue, maxValue+1);
+//     }
+//     return arr;
 // }
-// else Console.WriteLine("Нет");
 
+// int sumEvenNumbers(int[] arr) // Сумма четных чисел массива
+// {
+//     int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] % 2 == 0) count += 1;
+//     }
+//     return count;
+// }
 
+// int sumOddIndex(int[] arr) // Сумма элементов неченых индексов
+// {
+//     int count = 0;
+//     for (int i = 1; i < arr.Length; i=i+2)
+//     {
+//         count = count + arr[i];
+//     }
+//     return count;
+// }
 
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-
-// int num_x1 = int.Parse(Console.ReadLine());
-// int num_y1 = int.Parse(Console.ReadLine());
-// int num_z1 = int.Parse(Console.ReadLine());
-// int num_x2 = int.Parse(Console.ReadLine());
-// int num_y2 = int.Parse(Console.ReadLine());
-// int num_z2 = int.Parse(Console.ReadLine());
-// double result = Math.Sqrt(Math.Pow(num_x2-num_x1,2) + Math.Pow(num_y2-num_y1,2) + Math.Pow(num_z2-num_z1,2));
-// result = Math.Round(result, 2);
+// Console.WriteLine("ЗАДАНИЕ 1");
+// int[] arr1 = generateArray(6, 100, 998);
+// for (int i = 0; i < arr1.Length; i++)
+// {
+//     Console.Write(arr1[i] + ", ");
+// }
+// int result = sumEvenNumbers(arr1);
+// Console.WriteLine();
 // Console.WriteLine(result);
 
 
-
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
-
-// Console.WriteLine("Число: ");
-// int num = int.Parse(Console.ReadLine());
-// int result = 1;
-// for (int i = 1; i <= num; i++)
+// Console.WriteLine("ЗАДАНИЕ 2");
+// int[] arr2 = generateArray(8, -100, 99);
+// for (int i = 0; i < arr2.Length; i++)
 // {
-//     result = i * i * i;
-//     Console.WriteLine(result);
+//     Console.Write(arr2[i] + ", ");
 // }
+// Console.WriteLine();
+// int sum = sumOddIndex(arr2);
+// Console.WriteLine(sum);
