74bfeb7f75a45ae117caeb99b1615318f232cb60
Egor <egorkozlov1810@gmail.com>
2023-07-19 01:41:33 +0400

    5 семинар

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// int[] generateArray(int lenght, int minValue, int maxValue)  // Генерация массива
+﻿// int[] generateArray(int lenght, int minValue, int maxValue)  // Генерация массива типа int
 // {
 //     int [] arr = new int[lenght];
 //     for (int i = 0; i < arr.Length; i++)
@@ -6,6 +6,33 @@
 //         arr[i] = new Random().Next(minValue, maxValue+1);
 //     }
 //     return arr;
+// }
+
+// double[] generateDoubleArray(int lenght)  // Генерация массива типа double
+// {
+//     double [] arr = new double[lenght];
+//     Random rand = new Random();
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(-100, 100) + rand.NextDouble();
+//     }
+//     return arr;
+// }
+
+// double raznicaMaxMin(double[] arr) // Разница между максимальным и минимальным элементов массива double
+// {
+//     double temp1 = arr[0];
+//     double temp2 = arr[0];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if(arr[i] > temp1) temp1 = arr[i];
+//     }
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if(arr[i] < temp2) temp2 = arr[i];
+//     }
+
+//     return temp1 - temp2;
 // }
 
 // int sumEvenNumbers(int[] arr) // Сумма четных чисел массива
@@ -48,3 +75,13 @@
 // Console.WriteLine();
 // int sum = sumOddIndex(arr2);
 // Console.WriteLine(sum);
+
+
+// Console.WriteLine("ЗАДАНИЕ 3"); // Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// double[] arr3 = generateDoubleArray(5);
+// for (int i = 0; i <arr3.Length; i++)
+// {
+//     Console.Write(arr3[i] + ", ");
+// }
+// Console.WriteLine();
+// Console.WriteLine(raznicaMaxMin(arr3));
